723e18ed5426044b543a1a4dcbd8f54bfce93727
Moises Nessim <moises.nessim@topmanage.com>
2019-05-22 17:35:37 -0500

    Refactor default nix as function.

--- nix/default.nix
+++ nix/default.nix
@@ -1,25 +1,10 @@
- with import ./nixpkgs.nix;
-let 
-  updateVsCodeSettingsFile=true;
-  # ps1903 = import ./nixpkgs.nix;
-  #VSCode Settings file
-  #relative path from nix folder
-  vscodeSettingsFile = builtins.toString ../.vscode/settings.json;
-  updateVSCodeSettings =  
-    builtins.toJSON (
-      (builtins.fromJSON (builtins.readFile vscodeSettingsFile)) // 
-      { 
-        "java.home" = "${jdk}/"; 
-      }
-    );
-  #uses jq to beautify the json coming out of builtins.toJSON
-  vscodeUpdateSettingsCmd = lib.optionalString (lib.pathExists vscodeSettingsFile && updateVsCodeSettingsFile) ''
-    echo '====================================='
-    echo 'Updating ${vscodeSettingsFile} with JavaHome path'
-    echo '${updateVSCodeSettings}' | ${jq}/bin/jq .  > ${vscodeSettingsFile}
-  '';
-
-in
+ {
+   json-path?true, json-path-assert?true, json-path-web-test?true
+   ,additionalBuildInputs?[]
+   ,shellHook?""
+   ,pkgs?import ./nixpkgs.nix
+ }:
+with pkgs;
 stdenv.mkDerivation {
   name = "JsonPath";
   version = "2.4.0";
@@ -27,7 +12,7 @@ stdenv.mkDerivation {
   buildInputs = [
     jdk
     gradle_4_10
-  ];
+  ]++additionalBuildInputs;
 
   buildPhase = ''
     echo "Temp folder: " $(pwd)
@@ -40,14 +25,14 @@ stdenv.mkDerivation {
   installPhase = ''
     echo "Installing JsonPath..."
     mkdir -p $out
-    cp json-path/build/libs/json-path-2.4.0.jar $out
-    cp json-path-assert/build/libs/json-path-assert-2.4.0.jar $out
-    cp json-path-web-test/build/libs/json-path-web-test-2.4.0.jar $out
-    echo "JsonPath jars can be found here:" $out
-  '';
-
-  shellHook=''
-    ${vscodeUpdateSettingsCmd}
-  '';
+    ''
+    + lib.optionalString json-path ''cp json-path/build/libs/json-path-2.4.0.jar $out
+    ''
+    + lib.optionalString json-path-assert ''cp json-path-assert/build/libs/json-path-assert-2.4.0.jar $out
+    ''
+    + lib.optionalString json-path-web-test ''cp json-path-web-test/build/libs/json-path-web-test-2.4.0.jar $out
+    '';
+  
+  inherit shellHook;
 }
 
--- nix/shell.nix
+++ nix/shell.nix
@@ -1,9 +1,29 @@
 with import ./nixpkgs.nix;
+let
+
+  #VSCode Settings file
+  #relative path from nix folder
+  vscodeSettingsFile = builtins.toString ../.vscode/settings.json;
+  updateVSCodeSettings =  
+    builtins.toJSON (
+      (builtins.fromJSON (builtins.readFile vscodeSettingsFile)) // 
+      { 
+        "java.home" = "${jdk}/"; 
+      }
+    );
+  #uses jq to beautify the json coming out of builtins.toJSON
+  vscodeUpdateSettingsCmd = lib.optionalString (lib.pathExists vscodeSettingsFile) ''
+    echo '====================================='
+    echo 'Updating ${vscodeSettingsFile} with JavaHome path'
+    echo '${updateVSCodeSettings}' | ${jq}/bin/jq .  > ${vscodeSettingsFile}
+  '';
+in
 import ./default.nix {
   additionalBuildInputs = [
       figlet
     ];
-  shellHook = ''
+  shellHook = '' 
     figlet "JsonPath"
+    ${vscodeUpdateSettingsCmd}
   '';
 }
